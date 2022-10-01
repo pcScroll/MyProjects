f4d6bdb40b657737fb8af4bf0e56f8e37266db27
pcScroll <tturani@hotmail.com>
2022-10-02 01:20:58 +0400

    add do_search page

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from vsearch import search4letters
 app = Flask(__name__)
 """ ddddddddddddddddddddddd"""
 
+
 @app.route('/')
 def hello() -> str:
-    return 'Hello world from Flask!'
+    return 'Hello1 world from Flask!'
 
+
+@app.route('/search4')
+def do_search() -> str: 
+    return str(search4letters('life, the universe, and everything', 'eiru, !'))
 
 app.run()
